7c2fbf7a154156654a0aeb51f9aaf0949a28b2f7
Suzanne Kennedy <suzanne@evolvingweb.ca>
2014-12-03 18:23:21 -0500

    Initializing the repository for the menu_content module

---
+++ js/menu-content.js
@@ -0,0 +1,16 @@
+(function($) { Drupal.behaviors.menuContentDropdown = {
+    attach: function (context) {
+      $('#block-system-main-menu > div.content > ul.menu > li.expanded', context).once(function() {
+        //Get the current menu item's link
+        var item = $(this).find('a').attr('href').substring(1);
+        //Create the dropdown content object
+        var menuContent = Drupal.settings.menu_content[item];
+        //Get the current menu's submenus
+        var subMenu = $(this).find('> ul.menu');
+        //Add the submenu and the menu content
+        var dropdown = $('<div class="menu-content" />').append(menuContent).append(subMenu);
+        $(this).append(dropdown);
+      });
+    }
+  }
+})(jQuery);
